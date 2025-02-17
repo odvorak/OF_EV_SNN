f184d32028774f247abf0f8350c527965f0e3919
odvorak <54942164+odvorak@users.noreply.github.com>
2025-02-17 12:11:11 +0100

    change checkpoint

--- generate_model_prediction_landing.py
+++ generate_model_prediction_landing.py
@@ -33,7 +33,7 @@ for flight in flight_list:
     if not os.path.isdir(f'/root/results/tests/'):
         os.mkdir(f'/root/results/tests/')
 
-    checkpoint = f'/root/results/checkpoints_and_logs/checkpoint_l1_epoch30.pth'
+    checkpoint = f'/root/results/checkpoints_and_logs/checkpoint_l1_epoch32.pth'
     results_directory = f'/root/results11/{flight}/'
 
     if not os.path.isdir(results_directory):
