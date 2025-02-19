c28903e882701c65e6d1d134f5b9338be474e3cc
odvorak <54942164+odvorak@users.noreply.github.com>
2025-02-19 17:18:17 +0100

    typo

--- train_3dNet_cat_1res_200_small.py
+++ train_3dNet_cat_1res_200_small.py
@@ -81,7 +81,7 @@ valid_dataloader = torch.utils.data.DataLoader(dataset = valid_dataset, batch_si
 
 # Create the network
 
-net = NeuronPool_Separable_Pool3d_200(multiply_factor = 35.).to(device)
+net = NeuronPool_Separable_Pool3d_200_small(multiply_factor = 35.).to(device)
 trainable_params = sum(p.numel() for p in net.parameters() if p.requires_grad)
 print('Trainable parameters: {}'.format(trainable_params))
 
