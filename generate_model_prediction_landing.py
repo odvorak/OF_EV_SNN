e1f92dcf0c993f43a11673ff02ce5eb30790d327
odvorak <54942164+odvorak@users.noreply.github.com>
2025-02-19 20:16:45 +0100

    typo

--- generate_model_prediction_landing.py
+++ generate_model_prediction_landing.py
@@ -33,7 +33,7 @@ for flight in flight_list:
     if not os.path.isdir(f'/root/results/tests/'):
         os.mkdir(f'/root/results/tests/')
 
-    checkpoint = f'/root/results/checkpoints_and_logs/checkpoint_l1_epoch22.pth'
+    checkpoint = f'/root/results/checkpoints_and_logs/checkpoint_small_epoch34.pth'
     results_directory = f'/root/results/{flight}/'
 
     if not os.path.isdir(results_directory):
